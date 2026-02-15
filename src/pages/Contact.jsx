ccf7569436576dd0d73b4f244d1e7e0994957d7b
deepika <deepika2431307@gmail.com>
2026-02-16 00:42:49 +0530

    Update project

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,28 @@
+import "./page.css";
+
+export default function Contact() {
+  return (
+    <div className="page">
+      <div className="page-content">
+
+        <h1>Contact Us</h1>
+
+        <h2>Frontend Support</h2>
+        <p><b>Name:</b> Deepika</p>
+        <p><b>Email:</b> deepika2431307@gmail.com</p>
+         <p><b>Linkedin:</b> <a href=" https://www.linkedin.com/in/deepika-p8887/" target="_blank">Visit Profile</a></p>
+         <br/>
+        <h2>Backend Support</h2>
+        <p><b>Name:</b> Devansh Pal</p>
+        <p><b>Email:</b> devanshpal2430787@gmail.com</p>
+        <p><b>Linkedin:</b> <a href=" https://www.linkedin.com/in/deepika-p8887/" target="_blank">Visit Profile</a></p>
+        <br />
+        <p>
+          For any issues or questions, or collaboration opportunities,feel free to contact us via email or linkedin.<br/>
+          We are open to collaborations and happy to help!
+        </p>
+
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Debugger.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "../pages/Debugger.css";
+
+export default function Debugger() {
+  const [log, setLog] = useState("");
+  const [result, setResult] = useState("");
+
+  const analyzeLog = () => {
+    if (log.includes("npm install")) {
+      setResult("Dependency issue detected. Run: npm install");
+    } 
+    else if (log.includes("permission denied")) {
+      setResult("Permission error. Try running terminal as administrator.");
+    }
+    else {
+      setResult("No known issue detected. Please check YAML configuration.");
+    }
+  };
+
+  return (
+    <div className="debugger-page">
+
+      <h2>Paste Your CI/CD Error Logs</h2>
+
+      <textarea
+        placeholder="Paste Jenkins, GitHub Actions, Docker, or build error logs here..."
+        value={log}
+        onChange={(e) => setLog(e.target.value)}
+      />
+
+     <button className="analyse-btn" onClick={analyzeLog}>Analyze</button>
+
+      <div className="output">
+        <h3>Solutions</h3>
+        <p>{result}</p>
+      </div>
+
+    </div>
+  );
+}
